80110ff854e3bf4e287f1cb242ac4ef134d4f914
Mahendra singh <mahi07ms03@gmail.com>
2024-02-15 00:38:03 +0530

    a

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const AppError = require('./utils/appError');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
