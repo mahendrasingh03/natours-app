6598925d5d2e908d10663bb3457a09825246c293
Mahendra singh <mahi07ms03@gmail.com>
2024-04-06 09:20:25 +0530

    handlerFactory

--- routes/reviewRouts.js
+++ routes/reviewRouts.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
+// GET/tour/234fad4/reviews
+// GET/tour/234fad4/reviews
+// POST/reviews
 
 router
   .route('/')
@@ -11,5 +15,7 @@ router
     authController.restrictTo('user'),
     reviewController.createReview
   );
+
+router.route('/:id').delete(reviewController.deleteReview);
 
 module.exports = router;
