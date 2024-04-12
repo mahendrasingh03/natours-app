d0ffeda90d909d3b65cf94b8a599214f9e037959
Mahendra singh <mahi07ms03@gmail.com>
2024-04-12 20:25:51 +0530

    auth & auth

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,9 @@
+const { query } = require('express');
+const { populate } = require('../models/reviewModel');
 const AppError = require('../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const ExpressMongoSanitize = require('express-mongo-sanitize');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -39,6 +43,45 @@ exports.createOne = (Model) =>
 
     res.status(201).json({
       status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
       data: {
         data: doc,
       },
--- routes/reviewRouts.js
+++ routes/reviewRouts.js
@@ -7,11 +7,12 @@ const router = express.Router({ mergeParams: true });
 // GET/tour/234fad4/reviews
 // POST/reviews
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -19,7 +20,14 @@ router
 
 router
   .route('/:id')
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
