12e3be616961426a0c8dad39bc6da41da3ef6381
Mahendra singh <mahi07ms03@gmail.com>
2024-04-17 22:22:16 +0530

    redering

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRouts');
+const viewRouter = require('./routes/viewRouts');
+
 const app = express();
 
 app.set('view engine', 'pug');
@@ -76,14 +78,7 @@ app.use((req, res, next) => {
 // app.delete('/api/v1/tours/:id',deleteTour);
 
 // 3) ROUTES
-
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Mahi',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
